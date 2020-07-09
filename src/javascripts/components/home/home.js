1673557ba07abbd9d1148a277eaf64863069d91b
Michael Dotson <thedotson@icloud.com>
2020-07-08 19:48:25 -0500

    attempting to delete pins

--- src/javascripts/components/home/home.js
+++ src/javascripts/components/home/home.js
@@ -4,6 +4,7 @@ const homeBuilder = () => {
   const domString = `
   <div class="text-center mt-3">
     <h1>Pinterest</h1>
+    <h3>Please Login to view your boards</h3>
   </div>`;
 
   utils.printToDom('#home', domString);
--- src/javascripts/components/singleBoard/singleBoard.js
+++ src/javascripts/components/singleBoard/singleBoard.js
@@ -3,6 +3,16 @@ import utils from '../../helpers/utils';
 import divManip from '../divManip/divManip';
 import './singleBoard.scss';
 
+const deletePinEvent = (e) => {
+  const pinId = e.target.closest('.card').id;
+  pinData.deletePins(pinId)
+    .then(() => {
+      // eslint-disable-next-line no-use-before-define
+      buildSingleBoard(e);
+    })
+    .catch((err) => console.error(err));
+};
+
 const buildSingleBoard = (e) => {
   const boardId = e.target.closest('.card').id;
   divManip.showPins();
@@ -23,18 +33,19 @@ const buildSingleBoard = (e) => {
           </div>`;
         }
       });
-      domString += '</div>';
 
       domString += `
-      <div class="text-center mt-5">
-        <button class="btn btn-danger" id="back-button">Back</button>
-      </div>`;
+        </div>
+        <div class="text-center mt-5">
+          <button class="btn btn-danger" id="back-button">Back</button>
+        </div>`;
 
       utils.printToDom('#pins', domString);
       $('body').on('click', '#back-button', divManip.showBoards);
       $('body').on('click', '#back-button', divManip.hidePins);
       $('body').on('click', '#my-boards', divManip.showBoards);
       $('body').on('click', '#my-boards', divManip.hidePins);
+      $('body').on('click', '.delete-pin', deletePinEvent);
     })
     .catch((err) => console.error('singleBoards broke', err));
 };
